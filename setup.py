afab17549ca6efb9b8f217339ebd9fb42104c48f
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2025-01-12 21:26:26 -0500

    Package preparation

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from setuptools import setup
-
-with open("README.md", "r") as f:
-    long_description = f.read()
+from setuptools import setup, find_packages
 
 setup(
     name="discord.py-interaction-pagination",
-    version="0.0.6",
-    description="Easily create pagination for your embeds with interaction.",
-    long_description=long_description,
+    version="1.0.0",
+    description="Easily create pagination for your embeds with interaction (using discord.Interaction)",
+    long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
     py_modules=["Pagination"],
-    package_dir={'': "src"},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     url="https://github.com/DukeOfCheese/Pagination-Interaction",
     author="soosBot and DukeOfCheese",
     author_email="dukeofcheesebusiness@gmail.com",
@@ -26,5 +24,8 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
         'Typing :: Typed',
-    ]
+    ],
+    install_requires=[
+        "discord.py>=2.0.0",
+    ],
 )
